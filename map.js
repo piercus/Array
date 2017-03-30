5320a8393993a4d85ee905c42a7b5eba93048ee9
Pierre Colle <piercus@gmail.com>
2017-03-30 12:24:20 +0200

    do not ovverride map existing fn

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 (define("Array/map", function() {
 
-  Array.prototype.map = function(fn, scope) { // returns a new array where elements are fn(this[i])
+  Array.prototype.map || (Array.prototype.map = function(fn, scope) { // returns a new array where elements are fn(this[i])
   //scope is here for node's map compatibility
     if (scope){ fn = fn.bind(scope);}
     var r = this.slice(),i,n;
@@ -12,6 +12,6 @@
       for (i = 0, n = r.length; i < n; i++){ r[i] = r[i][fn]();}
     }
     return r;
-  };
-  
+  });
+
 }));
