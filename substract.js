2f9d8995dba9736056af48e81b8da23c424b3967
Colle <piercus@users.noreply.github.com>
2018-02-08 17:08:47 +0100

    Update substract.js

--- substract.js
+++ substract.js
@@ -1,3 +1,7 @@
+if (typeof define !== "function") {
+    var define = require("amdefine")(module);
+}
+
 (define("Array/substract", function() {
   
   Array.prototype.substract = function(o) { // same as sub but doesn't override this
