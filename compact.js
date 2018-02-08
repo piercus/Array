f2328203a3f9f0079de33ea78e618e6aaeb9dba7
Colle <piercus@users.noreply.github.com>
2018-02-08 17:01:01 +0100

    Update compact.js

--- compact.js
+++ compact.js
@@ -1,3 +1,7 @@
+if (typeof define !== "function") {
+    var define = require("amdefine")(module);
+}
+
 (define("Array/compact", function() {
   
   Array.prototype.compact = function() { // same as above but the "not-in-the-return" values are the one where f(el) is true
